27c4afd53f4f82cdc29aa99d74d6379784faec4c
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2026-09-02 11:52:39 -0500

    update

--- app.py
+++ app.py
@@ -140,4 +140,4 @@ def connect():
     socketio.start_background_task(background_thread)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
+    socketio.run(app, debug=True, host='0.0.0.0', port=5008)
